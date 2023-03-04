d531bc8a626824f985476c4853c1cbb930be1b18
ManueLucas <m.lucas2603@gmail.com>
2023-03-04 12:41:34 -0500

    -removed express js server

--- public/javascripts/eventlisteners.js
+++
@@ -1,5 +0,0 @@
-var form = document.getElementById("form");
-function handleForm(event) { event.preventDefault(); }
-form.addEventListener('submit', handleForm);document.addEventListener("submit", uploadBySubmit)
-console.log("test")
-//TODO: GET .JS FILE LINKING WORKING FIRST
