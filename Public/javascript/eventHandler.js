6e17f8c4dedbdaed9d3d709238abc04ce1af19f5
ManueLucas <m.lucas2603@gmail.com>
2023-03-05 09:05:14 -0500

    fixed double call to pixelgathering functions

--- Public/javascript/eventHandler.js
+++ Public/javascript/eventHandler.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('activate_camera_button').addEventListener('click', camera_button)
-    document.addEventListener('click', getPixelColor)
+    //document.addEventListener('click', getPixelColor)
     document.getElementById('screenshot').addEventListener('click', screenshot)
     document.getElementById('reset').addEventListener('click', reset)
     document.getElementById('canvas').addEventListener('click', getPixelColor)
